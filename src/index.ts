554e7655a617203572f79528616de40ef8a1a3be
skrsikop <skrsikop@gmail.com>
2026-01-19 07:34:22 +0300

    Implement all subjects

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import express  from 'express';
+import subjectsRouter from './routes/subjects';
+import cors from 'cors'
 
 const app = express();
 const port = 8000;
 
-app.use(express.json());
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}))
 
+app.use(express.json());
+app.use('/api/subjects', subjectsRouter)
 app.get("/", (req, res) => {
     res.send("Hello from backend!");
 });
