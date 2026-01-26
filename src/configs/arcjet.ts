6e608b7b8e9fbfe0e6c85335b1e90de37e5e0e22
skrsikop <skrsikop@gmail.com>
2026-01-26 16:56:01 +0300

    implement security middleware with arcjet

---
+++ src/configs/arcjet.ts
@@ -0,0 +1,27 @@
+import arcjet, { detectBot, shield, slidingWindow } from "@arcjet/node";
+
+if(!process.env.ARCJET_KEY && process.env.ARCJET_ENV === 'test') {
+    throw new Error("ARCJET_KEY env is required");
+}
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    shield({ mode: "LIVE" }),
+    detectBot({
+      mode: "LIVE", 
+      allow: [
+        "CATEGORY:SEARCH_ENGINE", 
+        "CATEGORY:PREVIEW", 
+      ],
+    }),
+
+    slidingWindow({
+      mode: "LIVE",
+      interval: '2s', 
+      max: 5, 
+    }),
+    
+  ],
+});
+
+export default aj;
